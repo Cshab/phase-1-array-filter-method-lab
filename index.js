24fe1053f2d2d0b6316aac850df19a6e4f7b7f91
Colby Shabel <jcshabel@gmail.com>
2023-02-09 10:32:02 -0600

    commit tree 9ca29ec938dfbced8e46129e40091a0367b9f112

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching (drivers,person){
+    
+    for(let person of drivers){
+        return drivers.filter.push(function(driver) {driver.toLowerCase() === person.toLowerCase()})
+    }
+}
